e007f66bf94afc265bef1b8b483578c596d8c130
mzacariasdionisio <mzacarias@net-consultores.com>
2026-08-31 20:46:31 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-cd /proyecto/credimax/
+cd /proyecto/Credimas/
 git pull origin main
 
 sudo bash -c '
@@ -9,7 +9,7 @@ sudo bash -c '
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
   nvm use 18.9.1
 
-  cd /proyecto/credimax/backend
+  cd /proyecto/Credimas/backend
   npm install --legacy-peer-deps
 
   pm2 restart 7
